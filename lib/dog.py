92f4cc3998fe1a677b47a7b60de1319ebe8ed887
bazanty <bazantyloffan@gmail.com>
2024-12-03 23:27:46 +0300

    dog and person

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def bark(self):
+        return f"{self.name} says Woof!"
+    def __init__(self, name): 
+        self.name = name
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+
+
+  
+        
+
+        
+    
+      
+
+
+ 
+
+
+pass
